2694ec982a7e8cb1b8715014b4e8766fc4b91ad1
Alberto Herrera <angelus2112@gmail.com>
2025-05-31 11:24:53 -0600

    Fechas de tarea 6 y 7

--- PythonUltimate11042025/controlador.py
+++ PythonUltimate11042025/controlador.py
@@ -47,7 +47,7 @@ class Controlador:
             "3": datetime(2025, 5, 2, 23, 59, 59, tzinfo=timezone.utc),
             "4": datetime(2025, 5, 9, 23, 59, 59, tzinfo=timezone.utc),
             "5": datetime(2025, 5, 9, 23, 59, 59, tzinfo=timezone.utc),
-            "6": datetime(2025, 5, 16, 23, 59, 59, tzinfo=timezone.utc),
-            "7": datetime(2025, 5, 16, 23, 59, 59, tzinfo=timezone.utc),
+            "6": datetime(2025, 6, 6, 23, 59, 59, tzinfo=timezone.utc),
+            "7": datetime(2025, 6, 6, 23, 59, 59, tzinfo=timezone.utc),
         }
         return datetime.now(tz=timezone.utc) - timedelta(hours=6) < fechas[tarea] 
