665ce7ad30908b7b0ba5c7621f2d9eeee6c29b26
Alberto Herrera <angelus2112@gmail.com>
2025-09-18 15:26:53 -0600

    Renombra

--- AnálisisDeSeriesDeTiempo/models/resultado.py
+++
@@ -1,6 +0,0 @@
-class Resultado:
-    def __init__(self, argumentos, esperado, obtenido, estado):
-        self.argumentos = argumentos
-        self.esperado = esperado
-        self.obtenido = obtenido
-        self.estado = estado
