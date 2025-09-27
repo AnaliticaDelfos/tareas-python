a76493d9af4721684a39185467f0418103fd45c3
Alberto Herrera <angelus2112@gmail.com>
2025-09-27 08:37:22 -0600

    Fechas en tareas

--- AutomatizacionDeExcel092025/controlador.py
+++ AutomatizacionDeExcel092025/controlador.py
@@ -41,13 +41,13 @@ class Controlador:
     @staticmethod
     def validar_fecha(tarea):
         fechas = {
-            "0": datetime(2025, 7, 8, 23, 59, 59, tzinfo=timezone.utc),
-            "1": datetime(2025, 7, 8, 23, 59, 59, tzinfo=timezone.utc),
-            "2": datetime(2025, 7, 8, 23, 59, 59, tzinfo=timezone.utc),
-            "3": datetime(2025, 7, 8, 23, 59, 59, tzinfo=timezone.utc),
-            "4": datetime(2025, 6, 29, 23, 59, 59, tzinfo=timezone.utc),
-            "5": datetime(2025, 6, 29, 23, 59, 59, tzinfo=timezone.utc),
-            "6": datetime(2025, 6, 29, 23, 59, 59, tzinfo=timezone.utc),
-            "7": datetime(2025, 6, 19, 23, 59, 59, tzinfo=timezone.utc),
+            "0": datetime(2025, 10, 4, 23, 59, 59, tzinfo=timezone.utc),
+            "1": datetime(2025, 10, 4, 23, 59, 59, tzinfo=timezone.utc),
+            "2": datetime(2025, 10, 11, 23, 59, 59, tzinfo=timezone.utc),
+            "3": datetime(2025, 10, 18, 23, 59, 59, tzinfo=timezone.utc),
+            "4": datetime(2025, 10, 25, 23, 59, 59, tzinfo=timezone.utc),
+            "5": datetime(2025, 11, 1, 23, 59, 59, tzinfo=timezone.utc),
+            "6": datetime(2025, 11, 8, 23, 59, 59, tzinfo=timezone.utc),
+            "7": datetime(2025, 11, 15, 23, 59, 59, tzinfo=timezone.utc),
         }
         return datetime.now(tz=timezone.utc) - timedelta(hours=6) < fechas[tarea]
