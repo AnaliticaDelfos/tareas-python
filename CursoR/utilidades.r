7f44fa352343b97e6463a2c7dfcab38b0e65f5cf
Alberto Herrera <angelus2112@gmail.com>
2026-04-22 23:31:10 -0600

    Calificador 11

---
+++ CursoR/utilidades.r
@@ -0,0 +1,18 @@
+library(httr)
+library(tidyverse)
+
+guardar_11 <- function(df) {
+    datos <- read_csv("cohorte_estudio.csv")
+    cabeza <- datos |> head()
+    son_iguales <- cabeza == df
+    calificacion <- 0
+    if (son_iguales) {
+        calificacion <- 1
+    }
+    response <- POST(
+        url = "https://calificar-r-435015279585.us-central1.run.app",
+        body = list(JPY_SESSION_NAME = Sys.getenv("JPY_SESSION_NAME"), usuario = Sys.getenv("JUPYTERHUB_USER"), calificacion = calificacion),
+        encode = "json" # Automatically sets Content-Type to application/json
+    )
+    content(response, "parsed")
+}
