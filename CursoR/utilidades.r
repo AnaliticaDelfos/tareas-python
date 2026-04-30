2558b5f459c4a39ace7e9f4f0d93361bcd33d92f
Alberto Herrera <angelus2112@gmail.com>
2026-04-29 23:35:52 -0600

    Notebook21

--- CursoR/utilidades.r
+++ CursoR/utilidades.r
@@ -228,3 +228,22 @@ guardar_20 <- function(df) {
     print("Calificación guardada")
     content(response, "parsed")
 }
+
+guardar_21 <- function(variable) {
+    resultado <- c("sexo", "glucosa_mgdl")
+    son_iguales <- variable %in% resultado
+    calificacion <- 0
+    if (son_iguales) {
+        print("Bien hecho :D")
+        calificacion <- 1
+    } else {
+        print("Vuélvelo a intentar :)")
+    }
+    response <- POST(
+        url = "https://calificar-r-435015279585.us-central1.run.app",
+        body = list(JPY_SESSION_NAME = "Notebook15", usuario = Sys.getenv("JUPYTERHUB_USER"), calificacion = calificacion),
+        encode = "json" # Automatically sets Content-Type to application/json
+    )
+    print("Calificación guardada")
+    content(response, "parsed")
+}
